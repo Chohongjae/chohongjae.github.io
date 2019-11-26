c0bde266b73c741fe54438455b464e77c612e445
chohongjae <bjq913@naver.com>
2019-11-26 11:24:47 +0900

    해시

---
+++ docs/Algorithm/해시/전화번호목록.py
@@ -0,0 +1,15 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42577
+def solution(phone_book):
+    answer = True
+    for i in phone_book:
+        for j in phone_book:
+            if i == j:
+                continue
+            if j.startswith(i):
+                return False
+
+    return answer
+
+
+if __name__ == "__main__":
+    solution()
