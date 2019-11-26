7c7113b54c7152ad14da591e9314a19b319b7b80
chohongjae <bjq913@naver.com>
2019-11-26 11:25:41 +0900

    스택, 큐

---
+++ docs/Algorithm/스택_큐/탑.py
@@ -0,0 +1,17 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42588
+def solution(heights):
+    answer = []
+    for idx, i in enumerate(reversed(heights)):
+        for idx2, j in enumerate(reversed(heights[:len(heights) - idx - 1])):
+            if i < j:
+                answer.insert(0, len(heights[:len(heights) - idx - 1]) - idx2)
+                break
+        else:
+            # for문이 끝나고 중간에 break가 되지 않으면 실행되는 for - else 이다.
+            answer.insert(0, 0)
+
+    return answer
+
+
+if __name__ == "__main__":
+    print(solution([1, 5, 3, 6, 7, 6, 5]))
