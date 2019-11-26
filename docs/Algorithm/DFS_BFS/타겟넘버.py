0ce43da3c1fd047042d2f479beaf3b60c4991c0a
chohongjae <bjq913@naver.com>
2019-11-26 11:35:03 +0900

    DFS / BFS

---
+++ docs/Algorithm/DFS_BFS/타겟넘버.py
@@ -0,0 +1,15 @@
+# https://programmers.co.kr/learn/courses/30/lessons/43165
+from itertools import product
+
+
+def solution(numbers, target):
+    answer = 0
+    for case in product(['+', '-'], repeat=len(numbers)):
+        i = sum([int(z[0] + str(z[1])) for z in zip(case, numbers)])
+        if i == target:
+            answer += 1
+    return answer
+
+
+if __name__ == "__main__":
+    solution([1, 1, 1, 1, 1], 3)
