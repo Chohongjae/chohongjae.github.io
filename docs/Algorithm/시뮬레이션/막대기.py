4f75755f23d33f1c3a5790d2b3c65e5cc445924d
chohongjae <bjq913@naver.com>
2019-11-26 11:32:35 +0900

    시뮬레이션

---
+++ docs/Algorithm/시뮬레이션/막대기.py
@@ -0,0 +1,17 @@
+# https://www.acmicpc.net/problem/1094
+def solution():
+    X = int(input())
+    a = [64]
+    while sum(a) > X:
+        b = a.pop()
+        a.append(b // 2)
+        a.append(b // 2)
+        if sum(a[:-1]) >= X:
+            a.pop()
+        elif sum(a[:-1]) == X:
+            break
+    return len(a)
+
+
+if __name__ == "__main__":
+    print(solution())
